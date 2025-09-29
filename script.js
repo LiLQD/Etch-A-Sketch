586ac0ee9b1b669cd5ac8ab916c9716a5d6d3a76
LiLQD <luuquangdung1511@gmail.com>
2025-09-29 22:42:12 +0700

    Add 16x16 divs grid

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.createElement("div")
+container.classList.add("container")
+const body = document.querySelector("body")
+
+
+for(let i = 0; i < 256; i++){
+    const squareDiv = document.createElement("div")
+    squareDiv.textContent = i + 1
+    squareDiv.classList.add("squareDivs")
+    container.appendChild(squareDiv)
+}
+body.appendChild(container)
