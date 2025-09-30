3c3a42ae2b01b124eb6003cad7e485bbb8fc5c9a
LiLQD <luuquangdung1511@gmail.com>
2025-09-30 21:15:22 +0700

    Move changeColorWhenHover function and append container out of all function

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ numberOfSquare.classList.add("numberOfSquareBtn")
 
 // Create n squares and add them to container
 function createSquareGrid(n) {
-    const containerSize = 750*750
-    let eachSquareLength = Math.sqrt(containerSize/n)
-
+    const containerSize = 750 * 750
+    let eachSquareLength = Math.sqrt(containerSize / n)
     for (let i = 0; i < n; i++) {
         const squareDiv = document.createElement("div")
         // squareDiv.textContent = i + 1
@@ -20,25 +19,19 @@ function createSquareGrid(n) {
         squareDiv.setAttribute("style", "width: " + eachSquareLength + "px", "height: " + eachSquareLength + "px")
         container.appendChild(squareDiv)
     }
-    body.appendChild(container)
 }
-
 // Change square color when mouse passes over
 function changeColorWhenHover() {
-    const squareDivGrid = document.querySelectorAll(".squareDivs")
-    squareDivGrid.forEach((elem) => {
-        elem.addEventListener("mouseover", (e) => {
-            e.target.classList.add("blueItems")
-        })
+    container.addEventListener("mouseover", (e) => {
+        e.target.classList.add("blueItems")
     })
 }
-
 // Take user input and power by 2
-function enterInput(){
+function enterInput() {
     let numberPerSide = 0
     while (true) {
         if (Number.isInteger(numberPerSide) === false || numberPerSide < 2 || numberPerSide > 100) {
-            let input = prompt("Number of square per side: ")
+            let input = prompt("Number of square per side (2 =< Input <= 100): ")
             numberPerSide = parseInt(input)
         }
         else break;
@@ -51,16 +44,12 @@ function enterInput(){
 // Add change number of square per side button
 numberOfSquare.textContent = "Change number of squares per side"
 btnDiv.appendChild(numberOfSquare)
-
-numberOfSquare.addEventListener("click", function() {
+numberOfSquare.addEventListener("click", function () {
     container.replaceChildren()
     createSquareGrid(enterInput())
-    changeColorWhenHover()
 })
 body.appendChild(btnDiv)
 
-
-
-createSquareGrid(Math.pow(64, 2))
+createSquareGrid(Math.pow(16,2))
 changeColorWhenHover()
-
+body.appendChild(container)
