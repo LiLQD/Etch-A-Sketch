5ecbf65324d12411295e106c2eecc727e79f5239
LiLQD <luuquangdung1511@gmail.com>
2025-09-30 20:45:03 +0700

    Add change number of square button

--- script.js
+++ script.js
@@ -1,19 +1,66 @@
 const container = document.createElement("div")
 container.classList.add("container")
+
 const body = document.querySelector("body")
 
+const btnDiv = document.createElement("div")
+btnDiv.classList.add("btnDiv")
+const numberOfSquare = document.createElement("button")
+numberOfSquare.classList.add("numberOfSquareBtn")
 
-for(let i = 0; i < 256; i++){
-    const squareDiv = document.createElement("div")
-    squareDiv.textContent = i + 1
-    squareDiv.classList.add("squareDivs")
-    container.appendChild(squareDiv)
+// Create n squares and add them to container
+function createSquareGrid(n) {
+    const containerSize = 750*750
+    let eachSquareLength = Math.sqrt(containerSize/n)
+
+    for (let i = 0; i < n; i++) {
+        const squareDiv = document.createElement("div")
+        // squareDiv.textContent = i + 1
+        squareDiv.classList.add("squareDivs")
+        squareDiv.setAttribute("style", "width: " + eachSquareLength + "px", "height: " + eachSquareLength + "px")
+        container.appendChild(squareDiv)
+    }
+    body.appendChild(container)
 }
-body.appendChild(container)
-const squareDivGrid = document.querySelectorAll(".squareDivs")
-squareDivGrid.forEach((elem) => {
-    elem.addEventListener("mouseover", (e) => {
-        e.target.classList.add("blueItems")
+
+// Change square color when mouse passes over
+function changeColorWhenHover() {
+    const squareDivGrid = document.querySelectorAll(".squareDivs")
+    squareDivGrid.forEach((elem) => {
+        elem.addEventListener("mouseover", (e) => {
+            e.target.classList.add("blueItems")
+        })
     })
+}
+
+// Take user input and power by 2
+function enterInput(){
+    let numberPerSide = 0
+    while (true) {
+        if (Number.isInteger(numberPerSide) === false || numberPerSide < 2 || numberPerSide > 100) {
+            let input = prompt("Number of square per side: ")
+            numberPerSide = parseInt(input)
+        }
+        else break;
+    }
+    let totalSquare = Math.pow(numberPerSide, 2)
+    console.log("Numper per side: " + numberPerSide)
+    console.log("Total square: " + totalSquare)
+    return totalSquare
+}
+// Add change number of square per side button
+numberOfSquare.textContent = "Change number of squares per side"
+btnDiv.appendChild(numberOfSquare)
+
+numberOfSquare.addEventListener("click", function() {
+    container.replaceChildren()
+    createSquareGrid(enterInput())
+    changeColorWhenHover()
 })
+body.appendChild(btnDiv)
+
+
+
+createSquareGrid(Math.pow(64, 2))
+changeColorWhenHover()
 
